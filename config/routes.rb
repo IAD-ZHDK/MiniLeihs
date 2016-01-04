e527522ac58d23b10bb6d4f670526de789b6dd4e
Joël Gähwiler <joel.gaehwiler@gmail.com>
2016-01-04 16:11:23 +0100

    expose sidekiq ui

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    username == ENV['AUTH_USERNAME'] && password == ENV['AUTH_PASSWORD']
+  end if Rails.env.production?
+  mount Sidekiq::Web => '/sidekiq'
+
   root 'root#index'
 
   get 'login' => 'root#login'
