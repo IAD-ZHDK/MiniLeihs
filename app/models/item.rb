db1b1ed40dbb45c535f8b45ea521534ab0152d70
Joël Gähwiler <joel.gaehwiler@gmail.com>
2016-01-06 13:15:41 +0100

    added borrowing terms

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,8 +7,9 @@ class Item
   field :borrower_email, type: String
   field :returned_at, type: Time
   field :last_notification_at, type: Time
+  field :agreed_to_terms, type: Boolean
 
-  validates_presence_of :description, :borrower_email
+  validates_presence_of :description, :borrower_email, :agreed_to_terms
 
   scope :borrowed, -> { where(returned_at: nil) }
   scope :returned, -> { where(:returned_at.exists => true) }
