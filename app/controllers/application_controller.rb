2025bd4abe001a9dc9167d27aef4d94dacffe2ee
Joël Gähwiler <joel.gaehwiler@gmail.com>
2016-01-06 13:28:23 +0100

    allow password as query parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,25 @@ class ApplicationController < ActionController::Base
   include ApplicationHelper
 
   protect_from_forgery with: :exception
+  before_action :check_auth
+
+  def check_auth
+    if params[:password]
+      login_with_password(params[:password])
+    end
+  end
+
+  def login_with_password(password)
+    if password == ENV['PASSWORD_ADMIN']
+      session[:user] = 'admin'
+      return true
+    elsif password == ENV['PASSWORD_TERMINAL']
+      session[:user] = 'terminal'
+      return true
+    end
+
+    false
+  end
 
   def check_user(*roles)
     return true if roles.include?(session[:user])
--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,11 +1,7 @@
 class AuthenticationController < ApplicationController
   def authenticate
-    if params[:login][:password] == ENV['PASSWORD_ADMIN']
-      session[:user] = 'admin'
-      flash[:notice] = 'Successfully logged in as admin!'
-    elsif params[:login][:password] == ENV['PASSWORD_TERMINAL']
-      session[:user] = 'terminal'
-      flash[:notice] = 'Successfully logged in as terminal!'
+    if login_with_password(params[:login][:password])
+      flash[:notice] = 'Successfully logged in!'
     end
 
     redirect_to :root
